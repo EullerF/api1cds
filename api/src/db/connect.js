f960847b5407a572dba6cb6371401b5fb334e245
Adriano Donisete Cassiano <adrianosenai604@gmail.com>
2026-03-31 13:06:11 -0300

    feat: add dependency env

--- api/src/db/connect.js
+++ api/src/db/connect.js
@@ -4,7 +4,7 @@ const pool = mysql.createPool({
     connectionLimit:10,
     host:'localhost', // Mude para o seu host (IP)
     user:'root', // Mude para o seu User do Mysql
-    password:'root', // Mude para a senha do seu User
+    password:'senai@604', // Mude para a senha do seu User
     database: 'vio' // Mude para o seu database JÁ criado
 })
 
